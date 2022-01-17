37f11df5e32b5661f0bd9fdd475de04d6115f88a
Siddhant Mishra <sidjmishra007@gmail.com>
2022-01-17 13:50:49 +0530

    Spotify Auth

--- music_recommend/lib/main.dart
+++ music_recommend/lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:music_recommend/recommendation.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MusicRecommend(),
-  ));
+  runApp(const MusicRecommend());
 }
 
-class MusicRecommend extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
