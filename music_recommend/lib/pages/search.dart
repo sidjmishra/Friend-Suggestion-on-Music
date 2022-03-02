7efd4c9e633076efe167962f702f45ba34acce9b
Siddhant Mishra <sidjmishra007@gmail.com>
2022-03-02 23:19:49 +0530

    New Search.dart

--- music_recommend/lib/pages/search.dart
+++ music_recommend/lib/pages/search.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: avoid_print, unnecessary_null_comparison
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -12,32 +13,41 @@ class Search extends StatefulWidget {
   _SearchState createState() => _SearchState();
 }
 
-class _SearchState extends State<Search> {
+class _SearchState extends State<Search>
+    with AutomaticKeepAliveClientMixin<Search> {
+  TextEditingController searchController = TextEditingController();
   late Future<QuerySnapshot> searchResultsFuture;
 
   handleSearch(String query) {
     Future<QuerySnapshot> users =
         usersRef.where('displayName', isGreaterThanOrEqualTo: query).get();
-
     setState(() {
       searchResultsFuture = users;
     });
+  }
+
+  clearSearch() {
+    searchController.clear();
   }
 
   AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
-          hintText: "Search for a user",
+          hintText: 'Search For A User...',
           filled: true,
           prefixIcon: const Icon(
             Icons.account_box,
-            size: 28,
+            size: 28.0,
+            color: Colors.red,
           ),
           suffixIcon: IconButton(
             icon: const Icon(Icons.clear),
-            onPressed: () => print("Cleared"),
+            onPressed: () {
+              clearSearch();
+            },
           ),
         ),
         onFieldSubmitted: handleSearch,
@@ -45,27 +55,28 @@ class _SearchState extends State<Search> {
     );
   }
 
-  SizedBox buildNoContent() {
+  buildNoContent() {
     final Orientation orientation = MediaQuery.of(context).orientation;
     return SizedBox(
       child: Center(
+        //So You WOnt Get Keyboard Error Because It Resize
         child: ListView(
           shrinkWrap: true,
           children: <Widget>[
             SvgPicture.asset(
               'assets/images/search.svg',
-              height: orientation == Orientation.portrait ? 300 : 200,
+              height: orientation == Orientation.portrait ? 300.0 : 200.0,
             ),
             const Text(
-              "Find Users",
+              'Find Users',
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.white,
                 fontStyle: FontStyle.italic,
                 fontWeight: FontWeight.w600,
-                fontSize: 60,
+                fontSize: 60.00,
               ),
-            ),
+            )
           ],
         ),
       ),
@@ -74,28 +85,32 @@ class _SearchState extends State<Search> {
 
   buildSearchResults() {
     return FutureBuilder(
-      future: searchResultsFuture,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return circularProgress();
-        }
-
-        List<Text> searchResults = [];
-        snapshot.data!.documents.forEach((doc) {
-          User user = User.fromDocument(doc);
-          searchResults.add(
-            Text(user.username),
+        future: searchResultsFuture,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          List<UserResult> searchResults = [];
+          snapshot.data.documents.forEach((doc) {
+            User user = User.fromDocument(doc);
+            UserResult searchResult = UserResult(user);
+            print('USER');
+            searchResults.add(searchResult);
+          });
+          return ListView(
+            children: searchResults,
           );
         });
-        return ListView(
-          children: searchResults,
-        );
-      },
-    );
   }
+
+  //AutomaticKeepAliveClientMixin Requirement #2
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
+    //AutomaticKeepAliveClientMixin Requirement #3
+    super.build(context);
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
       appBar: buildSearchField(),
@@ -106,10 +121,40 @@ class _SearchState extends State<Search> {
 }
 
 class UserResult extends StatelessWidget {
-  const UserResult({Key? key}) : super(key: key);
+  final User user;
+
+  const UserResult(this.user, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text("User Result");
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.7),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => showProfile(context, profileId: user.id),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+              ),
+              title: Text(
+                user.displayName,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                user.username,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          const Divider(
+            height: 2.0,
+            color: Colors.white54,
+          )
+        ],
+      ),
+    );
   }
 }
